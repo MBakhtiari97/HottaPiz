eb077e0bc0dcb46221f6db6e50e1a4c215465047
Mohammad Bakhtiari <mohammadba86@yahoo.com>
2022-06-29 13:35:42 +0430

    Implemented Test Class

---
+++ HottaPiz.Infrastructure/Utilities/Generator/Generator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HottaPiz.Infrastructure.Utilities.Generator
+{
+    public static class Generator
+    {
+        public static int UniqueNumberGenerator()
+        {
+            Random rnd = new Random();
+            var number = rnd.Next(1111111, 9999999);
+            return number;
+        }
+    }
+}
---
+++ HottaPiz/Tests/TestGenerators.cs
@@ -0,0 +1,23 @@
+﻿using HottaPiz.Infrastructure.Utilities.Generator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HottaPiz.Web.Tests
+{
+    [TestClass]
+    public class TestGenerators
+    {
+        public TestGenerators()
+        {
+            
+        }
+
+        [TestMethod]
+        public void ShouldNotBeNull()
+        {
+            //Act
+            var generatedNumber = Generator.UniqueNumberGenerator();
+            //Assert
+            Assert.IsNotNull(generatedNumber);
+        }
+    }
+}
